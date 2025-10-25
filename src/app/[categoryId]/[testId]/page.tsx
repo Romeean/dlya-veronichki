b27d332879cb26f15fe0d54eb618363399fc2f11
= <=>
2025-10-25 23:11:49 +0300

    Сделал логику отображения информации о тесте перед тем как его начать.

--- src/app/[categoryId]/[testId]/page.tsx
+++ src/app/[categoryId]/[testId]/page.tsx
@@ -1,3 +1,4 @@
+import { getQuizzes } from "@/action/getQuizzes";
 import Link from "next/link";
 
 export default async function Page({ params }: { params: { categoryId: string; testId: string } }) {
@@ -6,9 +7,17 @@ export default async function Page({ params }: { params: { categoryId: string; t
   const categoryId = awaitedParams.categoryId;
   const testId = awaitedParams.testId;
 
+  const quizzes = await getQuizzes(testId);
+  const currentQuiz = quizzes[0]
+  console.log(quizzes)
+
+
   return (
     <div>
-      <Link href={`/${categoryId}/${testId}/1`}>Почати тест</Link>
+      <p>{currentQuiz.title}</p>
+      <p>Кількість питань: {currentQuiz.tasks.length}</p>
+      <Link href={`/${categoryId}/${testId}/1`}>Почати тест </Link>
+
     </div>
   );
 }
