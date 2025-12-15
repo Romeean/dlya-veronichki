dba485931e069c0ebfb524ed023dfe493eab82b5
Roman <topmanov@gmail.com>
2025-12-15 18:10:14 +0200

    Обновил конфиг и схему

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,20 @@
 import { PrismaClient } from "../app/generated/prisma";
-import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL
+})
 
-const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+function prismaClientSingleton(){
+  return new PrismaClient({
+    adapter
+  })
+} 
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
+
+if(process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma
