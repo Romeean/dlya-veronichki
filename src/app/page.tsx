e46821d62dec5c231e8882ec5897ed4fd6e740fd
Roman <topmanov@gmail.com>
2025-12-17 22:55:00 +0200

    Подготовление новогоднего орнамента

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
 import { cookies } from "next/headers";
 import { Hero } from "./components/Hero";
 import { SubjectBlock } from "./components/SubjectBlock";
+import { Toy } from "./components/christmas/Toy";
 
 export default async function Home() {
   const userCookies = await cookies();
   const userDefiner = userCookies.get("unique-definer");
   console.log(userDefiner);
   return (
-    <div className="flex items-center justify-center min-h-screen flex-col">
+    <div className="flex items-center justify-center min-h-screen flex-col max-h-screen">
       <Hero />
       <SubjectBlock />
+      <Toy 
+        width={"70px"}
+        height={"100px"}
+        color={"red"}
+      />
     </div>
   );
 }
