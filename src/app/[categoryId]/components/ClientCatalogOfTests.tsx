7a230aabfb1d7a58426569bbb06c9f8d127cf855
Roman <topmanov@gmail.com>
2026-01-18 10:58:20 +0200

    Подвязка user к его личным ответам

--- src/app/[categoryId]/components/ClientCatalogOfTests.tsx
+++ src/app/[categoryId]/components/ClientCatalogOfTests.tsx
@@ -16,14 +16,8 @@ type ClientCatalogOfTestsProps = {
 export function ClientCatalogOfTests({ quizzes }: ClientCatalogOfTestsProps) {
   return (
     <div className="w-full min-h- flex justify-center ">
-      <div className="max-w-5xl">
-        <div className="flex min-h-screen w-full items-center justify-center">
-          <h1 className="text-3xl text-center text-gray-200">
-            В цій категорії ми детально будемо вивчати на практиці завдання з математики, у кожного тесту, є своя складність,
-            подивись на зірочки, чим більше зірочок - тим важче.
-          </h1>
-        </div>
-        <div id="tests">
+      <div className="max-w-5xl w-full">
+        <div id="tests" className="flex flex-row ">
           {quizzes.map((element, index) => (
             <Link key={index} href={`/${element.categoryId}/${element.testId}`} className="rounded-2xl min-h-32 p-4">
               <div className="rounded-2xl p-6 text-white bg-[#1e2939] shadow-[0_4px_20px_rgba(0,0,0,0.08)] transition-transform duration-200 hover:-translate-y-1 cursor-pointer flex flex-col justify-center h-48 w-full">
